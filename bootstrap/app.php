5be4a1d83650d44b7a8790fca20023607bcae168
ole <roslanamier27@gmail.com>
2025-05-10 18:24:13 +0800

    setup csrf bypass

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        $middleware->validateCsrfTokens(except: [
+            '/api/*',
+            '/*'
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
